154cffbd089d61c9c8401b76b62f1c075e7bcc1c
William Husein <william.husein@gmail.com>
2024-10-05 14:34:12 +0800

    Use proper gradle init

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,9 @@
-import cats.effect._
-import org.http4s._
-import org.http4s.dsl.io._
-import org.http4s.circe._
-import io.circe.generic.auto._
+import cats.effect.*
+import io.circe.Encoder
+import org.http4s.*
+import org.http4s.dsl.io.*
+import org.http4s.circe.*
+import io.circe.generic.auto.*
 import org.http4s.server.blaze.BlazeServerBuilder
 
 object Main extends IOApp {
@@ -11,7 +12,11 @@ object Main extends IOApp {
   case class Greeting(message: String)
 
   // Circe entity encoder to automatically convert Scala case class to JSON
-  implicit val greetingEncoder: EntityEncoder[IO, Greeting] = jsonEncoderOf[IO, Greeting]
+  implicit val greetingEncoder: Encoder[Greeting] = Encoder.forProduct1("message")(_.message)
+
+  // Circe-aware entity encoder for HTTP4s
+  implicit val greetingEntityEncoder: EntityEncoder[IO, Greeting] = jsonEncoderOf[IO, Greeting]
+
 
   // Define the route
   val helloWorldService = HttpRoutes.of[IO] {
